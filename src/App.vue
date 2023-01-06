1df7fc36df81ce17a1ef9d470c32688d1db03987
Alexander Klementev <klementev@greensight.ru>
2023-01-06 14:40:42 +0300

    Vue init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TheGame from "./components/TheGame.vue";
+
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <header>
+    <h1>Tic Tac Toe</h1>
+  </header>
+
+  <main>
+    <TheGame />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/TheGame.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+</script>
+
+<template>
+  <DocumentationIcon />
+</template>
